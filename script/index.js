7c8467d18ca2c31e7b501f59af5917ba6cb07d05
Wareezy <warrenjaftha16@gmail.com>
2023-11-10 10:46:32 +0200

    warrenJaftha

---
+++ script/index.js
@@ -0,0 +1,28 @@
+// Make a variable with a data of a string and check the type of this variable.
+let name="Warren"
+console.log(typeof name)
+
+// Reassign the value to a number and check the type again. Did it change?
+//yes it did change
+name=42
+console.log(typeof name)
+
+// Go ahead and create a variable with every data type you know of. Double check that you covered all of them by using the typeof check.
+let numberVar = 42;
+let stringVar = "Hello, Data Types!";
+let booleanVar = true;
+let arrayVar = [1, 2, 3];
+let objectVar = { key: "value" };
+let nullVar = null;
+let undefinedVar = undefined;
+let functionVar = function() {};
+
+
+console.log(typeof numberVar);    
+console.log(typeof stringVar);    
+console.log(typeof booleanVar);   
+console.log(typeof arrayVar);   
+console.log(typeof objectVar);   
+console.log(typeof nullVar);      
+console.log(typeof undefinedVar);
+console.log(typeof functionVar);
